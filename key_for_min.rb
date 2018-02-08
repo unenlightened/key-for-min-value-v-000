b2c828db705a66eedc2c2642614267c069ce4eca
Chely Ho <adept_inept@yahoo.com>
2018-02-08 07:56:00 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   min_key = nil
 
   name_hash.each do |key, value|
-    if min_key.nil? || value < name_hash[min_key]  # order matters. nil needs to be first test. why?
+    if min_key.nil? || value < name_hash[min_key]  # order matters. nil needs to be first test. why? because min_key doesn't exist yet
       min_key = key
     end
   end
