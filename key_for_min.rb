e447d4463676e73f56c80ff56783c4d4edba38da
Chely Ho <adept_inept@yahoo.com>
2018-02-08 07:51:08 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,8 +5,7 @@ def key_for_min_value(name_hash)
   min_key = nil
 
   name_hash.each do |key, value|
-    min_key = key if min_key.nil?
-    if value < name_hash[min_key]
+    if min_key.nil? || value < name_hash[min_key]  # order matters. nil needs to be first test. why?
       min_key = key
     end
   end
